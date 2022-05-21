4fa4547135ed64942bd12eb8efc9cd7835fb283c
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-21 13:21:55 +0530

    yes me

--- Http/index.js
+++ Http/index.js
@@ -7,7 +7,7 @@ const http = require('http');
 // }
 http.createServer((req, res) => {
     // res.write("Hello this is Mohammad kaif");
-    res.write("<h1>Hello this is Mohammad kaif</h1>");
+    res.write("<h1>Hello this s Mohammad kaif</h1>");
     res.end();
 }).listen(4500);
 PORT = 9100
