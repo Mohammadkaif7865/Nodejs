d553e73772f82d46e2e18a469f86e04287af539f
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-21 11:18:42 +0530

    yes me

---
+++ app.js
@@ -0,0 +1,5 @@
+module.exports = {
+    pi: 3.14,
+    y: 20,
+    x: function() { return "Hello world!!" }
+}
---
+++ try.js
@@ -0,0 +1,5 @@
+const app = require('./app'); //import file
+var a = 34;
+var b = 23;
+console.log(a * app.pi + b);
+console.log(app.x());
