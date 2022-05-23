8a0741f46277c173b88a3386713ccdf4eab7fbcd
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-23 21:29:22 +0530

    first commit

--- Http/htmlrender.js
+++ Http/htmlrender.js
@@ -1,3 +1,4 @@
+//Express
 const express = require('express');
 const app = express();
 let name;
@@ -6,13 +7,14 @@ app.get('', (req, res) => {
     <a href="/about">go to form<a>`);
 });
 app.get('/about', (req, res) => {
-    console.log("Browser send us this =>>>>", req.query.name);
+    console.log("Browser send us this =>>>>", req.query.name); //late we will know how can we display it as string literals
     res.send(`<h1>Hi ${req.query.name}</h1>
     <form>
     <label for="myName">Name :</label>
     <input type="text" name="name" id="myName" placeholder  = "Your name">
     <button type="submit">Submit</button>
     </form>
+    <a href="/">go to home</a>
     `);
 });
 app.get('/help', (req, res) => {
