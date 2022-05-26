7afb6b53b9e8e9bc00431d1ab85eba298271af36
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-26 12:53:02 +0530

    yes me

--- Http/mongoose.js
+++ Http/mongoose.js
@@ -9,4 +9,20 @@ app.post('/create', async(req, res) => {
     console.log(result);
     res.send(result);
 });
+app.get('/list', async(req, res) => {
+    let data = await Product.find();
+    res.send(data);
+});
+app.delete('/delete/:_id', async(req, res) => {
+    // console.log(req.params);
+    // res.send("Done");
+    let data = await Product.deleteOne(req.params);
+    console.log(data);
+    res.send(data);
+});
+app.put('/edit/:_id', async(req, res) => {
+    let data = await Product.updateOne(req.params, { $set: req.body });
+    console.log(data);
+    res.send(data);
+})
 app.listen(5000);
