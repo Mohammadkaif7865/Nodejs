d5d9bfb07ee7fe2799a8520c1932e1350bb7c55e
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-05-26 11:41:06 +0530

    yes me

---
+++ Http/product.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+    name: String,
+    brand: String,
+    category: String,
+    price: Number,
+});
+module.exports = mongoose.model('products', ProductSchema);
